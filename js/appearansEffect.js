607d9e972d30f90620e714ace3efb332480adfe6
Motaz-Mukhtar <motazmukhtar0@gmail.com>
2024-08-11 21:12:43 +0300

    some modification at home page

--- js/appearansEffect.js
+++ js/appearansEffect.js
@@ -2,10 +2,25 @@ const headerContainer = document.querySelectorAll(".up");
 
 console.log(headerContainer);
 
+
+function containersAppear() {
+    let index = 0;
+    window.addEventListener('scroll', () => {
+        if((headerContainer[index].offsetTop - 600) <= window.scrollY) {
+            headerContainer[index].style.cssText = `
+            -webkit-transform: translateY(0px);
+            transform: translateY(0px);
+            opacity: 1;
+            transition: 1s;
+            `;
+            index++;
+        }
+    })
+}
+
 window.addEventListener('load', () => {
     document.querySelector('.load-container').style.cssText = `
-        opacity: 0;
-        width: 0%;
+        width: 50%;
         transition: 2s;
     `;
     headerContainer[0].style.cssText = `
@@ -22,23 +37,5 @@ window.addEventListener('load', () => {
         transition: 1s;
         transition-delay: 0.2s;
     `;
-
 });
-
-function containersAppear() {
-    let index = 0;
-    window.addEventListener('scroll', () => {
-        if((headerContainer[index].offsetTop - 600) <= window.scrollY) {
-            headerContainer[index].style.cssText = `
-                -webkit-transform: translateY(0px);
-                transform: translateY(0px);
-                opacity: 1;
-                transition: 1s;
-            `;
-            index++;
-        }
-    })
-}
-
-
 containersAppear();
