e67d27a4c19cc2575d99220271130efba6ce60dd
Motaz-Mukhtar <motazksa2@gmail.com>
2024-09-10 14:30:11 +0300

    Fix the not found error in lang switch

--- js/langSwitch.js
+++ js/langSwitch.js
@@ -3,7 +3,16 @@ langSwitcher = document.querySelector('.lang-switcher');
 function switchLang () {
     let currentURL = window.location.href.split('/');
 
-    currentURL[3] = langSwitcher.value;
+    // currentURL[3] = langSwitcher.value;
+
+    // console.log(currentURL);
+    for (let i in currentURL) {
+        if (currentURL[i] === 'en') {
+            currentURL[i] = langSwitcher.value;
+        } else if (currentURL[i] === 'ar') {
+            currentURL[i] = langSwitcher.value;
+        }
+    }
 
     window.location.href = currentURL.join('/');
     window.history.replaceState(null, null, currentURL.join('/'));
