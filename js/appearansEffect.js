121b8a162a6b1b704593930363fb6138cd0e6971
Motaz-Mukhtar <motazksa2@gmail.com>
2024-11-11 12:34:26 +0300

    Modify css files.

--- js/appearansEffect.js
+++ js/appearansEffect.js
@@ -59,7 +59,7 @@ function navBarDropdown() {
     nodeNavBar.style.padding = '20px 10px';
 
     nodeNavBar.querySelector('.logo').style.margin = '0px';
-    nodeNavBar.querySelector('.logo img').src = '/assets/main-logo.png';
+    nodeNavBar.querySelector('.logo img').src = '/assets/main-logo-white.png';
     nodeNavBar.querySelectorAll('.sub-menu')[0].style.bottom = '-180px';
     nodeNavBar.querySelectorAll('.sub-menu')[1].style.bottom = '-180px';
 
