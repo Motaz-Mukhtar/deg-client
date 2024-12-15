e9079c551d164991963344f190460bd3fccee255
Motaz-Mukhtar <motazksa2@gmail.com>
2024-12-16 00:32:22 +0300

    Create 'Request Service' Page.

--- js/appearansEffect.js
+++ js/appearansEffect.js
@@ -60,19 +60,20 @@ function navBarDropdown() {
     nodeNavBar.querySelectorAll('.sub-menu')[0].style.bottom = '-180px';
     nodeNavBar.querySelectorAll('.sub-menu')[1].style.bottom = '-180px';
 
+    
     nodeNavBar.style.color = '#8f8f8f';
     nodeNavBar.childNodes[3].childNodes[3].querySelectorAll('li a')[0].style.color = '#8f8f8f';
     nodeNavBar.childNodes[3].childNodes[3].querySelectorAll('li a')[1].style.color = '#8f8f8f';
-    nodeNavBar.childNodes[3].childNodes[3].querySelectorAll('li a')[5].style.color = '#8f8f8f';
-    nodeNavBar.childNodes[3].childNodes[3].querySelectorAll('li a')[30].style.color = '#8f8f8f';
+    nodeNavBar.childNodes[3].childNodes[3].querySelectorAll('li a')[5].style.color = '#8f8f8f'
+    nodeNavBar.childNodes[3].childNodes[3].querySelectorAll('li a')[6].style.color = '#8f8f8f';
     nodeNavBar.childNodes[3].childNodes[3].querySelectorAll('li a')[31].style.color = '#8f8f8f';
     nodeNavBar.childNodes[3].childNodes[3].querySelectorAll('li a')[32].style.color = '#8f8f8f';
+    nodeNavBar.childNodes[3].childNodes[3].querySelectorAll('li a')[33].style.color = '#8f8f8f';
     nodeNavBar.childNodes[3].childNodes[3].lastChild.previousSibling.style.borderLeftColor = '#8f8f8f';
     nodeNavBar.childNodes[3].childNodes[3].lastChild.previousSibling.style.borderRightColor = '#8f8f8f';
 
-
     document.body.insertBefore(nodeNavBar, document.body.firstChild)
-    
+
     window.addEventListener('scroll', () => {
         if (innerWidth < 950) {
             nodeNavBar.childNodes[3].childNodes[3].lastChild.previousSibling.childNodes[1].style.color = 'var(--text-color)';
--- js/requestServiceScript.js
+++ js/requestServiceScript.js
@@ -1,9 +1,23 @@
+function setError(msg) {
+    const errorMessage = document.querySelector('.error-message');
+
+    errorMessage.innerText = msg;
+}
+
+function setSuccess(msg) {
+
+    document.querySelector('.join-us-section').style.display = 'none';
+    document.querySelector('.s1').style.display = 'none';
+
+    document.querySelector('.success-message').style.display = 'block';
+}
+
 function getSelectOptions(select) {
     const selectedValues = []
 
     for (const option of select) {
-        if (option.selected)
-            selectedValues.push(option.value.split('-').join(' '));
+        if (option.checked)
+            selectedValues.push(option.value);
     }
 
     return selectedValues;
@@ -11,20 +25,34 @@ function getSelectOptions(select) {
 
 document.querySelector('form').addEventListener('submit', async (e) => {
     e.preventDefault();
+    setError('');
 
-    const form = e.target;
     const submitButton = document.querySelector('.submit-btn');
-    const errorMessage = document.querySelector('.error-message');
-    const successMessage = document.querySelector('.success-message');
 
 
     const fullName = document.querySelector('#full-name').value.trim();
     const email = document.querySelector('#user-email').value.trim();
     const phoneNumber = document.querySelector('#phone-number').value.trim();
-    const services = getSelectOptions(document.querySelector('#services'));
-    const notes = document.querySelector('#user-notes').value.trim();
+    const services = getSelectOptions(document.querySelectorAll('.checkbox'));
 
+    if (services.length === 0) {
+        const currentURL = window.location.href.split('/');
+
+        if (currentURL[3] === "en") 
+            return setError("Choose at least 1 service");
+        else
+            return setError("إختر خدمة واحدة على الأقل")
+    }
+
+
+    const notes = document.querySelector('#user-notes').value.trim();
     const formData = new FormData();
+    const loaderButton = document.querySelector('.btn-loader');
+    const textButton = document.querySelector('.btn-text');
+
+    textButton.style.display = 'none';
+    loaderButton.style.display = 'block';
+    submitButton.disabled = true;
 
     formData.append('fullName', fullName);
     formData.append('email', email);
@@ -32,13 +60,25 @@ document.querySelector('form').addEventListener('submit', async (e) => {
     formData.append('services', services);
     formData.append('notes', notes ? notes : undefined);
 
-    await fetch('http://localhost:5000/api/v1/mail/services', {
+    await fetch('https://api.gec.sa/api/v1/mail/services', {
         method: 'POST',
         body: formData
     })
     .then(response => {
         console.log(response)
+        setSuccess('Request sent successfully!');
     })
-    .catch(error => console.log(error.message, error));
+    .catch(error => {
+        console.log(error);
+        const currentURL = window.location.href.split('/');
 
+        if (currentURL[3] === "en") 
+            return setError("Request failed to send");
+        else
+            return setError("فشل إرسال الطلب");
+    });
+
+    textButton.style.display = 'block';
+    loaderButton.style.display = 'none';
+    submitButton.disabled = false;
 });
