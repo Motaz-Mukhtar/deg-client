3ced629cafbd27b3f7ac7e293c1562f685304356
Motaz-Mukhtar <motazksa2@gmail.com>
2025-03-24 01:17:52 +0300

    Reduce number of js files.

--- js/appearansEffect.js
+++ js/appearansEffect.js
@@ -1,5 +1,6 @@
 const headerContainer = document.querySelectorAll(".up");
 
+// Container appearance Start
 function containersAppear() {
     let index = 0;
     window.addEventListener('scroll', () => {
@@ -40,7 +41,9 @@ window.addEventListener('load', () => {
     `;
 });
 
-// navBarDropdown
+// Container appearance End
+
+// navBarDropdown Start
 
 function navBarDropdown() {
     const navBar = document.querySelector('header');
@@ -56,12 +59,12 @@ function navBarDropdown() {
     nodeNavBar.style.backgroundColor = 'var(--background-color)'
     nodeNavBar.style.margin = '0px'
     nodeNavBar.style.padding = '10px';
-
+    
     nodeNavBar.querySelector('.logo').style.margin = '0px';
     nodeNavBar.querySelector('.logo img').src = '/assets/main-logo-yellow.png';
     nodeNavBar.querySelectorAll('.sub-menu')[0].style.bottom = '-180px';
     nodeNavBar.querySelectorAll('.sub-menu')[1].style.bottom = '-180px';
-
+    
     
     nodeNavBar.style.color = '#8f8f8f';
     nodeNavBar.childNodes[3].childNodes[3].querySelectorAll('li a')[0].style.color = '#8f8f8f';
@@ -73,9 +76,9 @@ function navBarDropdown() {
     nodeNavBar.childNodes[3].childNodes[3].querySelectorAll('li a')[33].style.color = '#8f8f8f';
     nodeNavBar.childNodes[3].childNodes[3].lastChild.previousSibling.style.borderLeftColor = '#8f8f8f';
     nodeNavBar.childNodes[3].childNodes[3].lastChild.previousSibling.style.borderRightColor = '#8f8f8f';
-
+    
     document.body.insertBefore(nodeNavBar, document.body.firstChild)
-
+    
     window.addEventListener('scroll', () => {
         if (innerWidth < 950) {
             nodeNavBar.childNodes[3].childNodes[3].lastChild.previousSibling.childNodes[1].style.color = 'var(--text-color)';
@@ -87,29 +90,41 @@ function navBarDropdown() {
         } else if (window.scrollY <= 200) {
             nodeNavBar.style.top = '-200px'
         }
-
+        
         // // Get the current scroll width.
         // let currentScroll = window.scrollY;
         
         // if (innerWidth < 950) {
-        //     nodeNavBar.childNodes[3].childNodes[3].lastChild.previousSibling.childNodes[1].style.color = 'var(--text-color)';
+            //     nodeNavBar.childNodes[3].childNodes[3].lastChild.previousSibling.childNodes[1].style.color = 'var(--text-color)';
         // } else {
         //     nodeNavBar.childNodes[3].childNodes[3].lastChild.previousSibling.childNodes[1].style.color = 'var(--second-maisn-color)';
         // }
-
+        
         // if (currentScroll > lastScrollY || currentScroll < 150) {
-        //     // navigation bar disappear when the user scroll down.
+            //     // navigation bar disappear when the user scroll down.
         //     nodeNavBar.style.top = '-200px';
         // } else {
         //     // navigation bar appear when the user scroll up.
         //     nodeNavBar.style.top = '0px'
         // }
-
+        
         // // Update the last scroll value.
         // lastScrollY = currentScroll;
     });
 
 }
+// navBarDropdown End
+
+// Menu Bar appearance start
+function openMenu(menuBar) {
+    menuBar.nextSibling.nextSibling.style.right = '-200px';
+}
+
+function closeMenu(closeButton) {
+    closeButton.parentElement.style.right = '-600px';
+}
+// Menu Bar appearance end
+
 
 containersAppear();
 navBarDropdown();
--- js/menuBarEffect.js
+++
@@ -1,8 +0,0 @@
-
-function openMenu(menuBar) {
-    menuBar.nextSibling.nextSibling.style.right = '-200px';
-}
-
-function closeMenu(closeButton) {
-    closeButton.parentElement.style.right = '-600px';
-}
